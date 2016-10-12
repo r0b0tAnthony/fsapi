e7d53ad7d1e106f26cf94ae7923f8efef60f2772
robotAnthony <anthony@robottalk.tv>
2016-10-12 15:45:02 -0400

    Added test of Projects

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 from user.user import user
 from acl.aclschema import aclSchema
+from project.project import project
 import json
 import pprint
-newUser = user('anthony', ['createProject', 'deleteProject', ])
+import posixpath
+newUser = user('anthony', ['createProject', 'deleteProject'], None, 13393583401)
 print newUser.getPermissions()
 
-newUser2 = user('tommy', ['deleteProject'], 'fhs8j2jahnvs')
+newUser2 = user('tommy', ['deleteProject'], 'fhs8j2jahnvs', 35097091740270)
 print newUser2.getPermissions()
 if newUser2.getPermission('createProject'):
     print 'Can CreateProject'
@@ -15,9 +17,16 @@ else:
 perm_fo = open('./foobar.json', 'r')
 perm_obj = json.load(perm_fo)
 
-schema = aclSchema('schema01', perm_obj)
+schema = aclSchema('schema01', perm_obj, 45823058208)
 for key, value in schema.getDACL().iteritems():
     pprint.pprint(key)
     acl = value['acl']
     pprint.pprint(acl.getACL())
 pprint.pprint(schema.getExpandedDACL())
+
+newProject = project('Project02',
+    {'linux': '/mnt/JOBS/16002_MR_ROBOT', 'darwin': '/volumes/JOBS/16002_MR_ROBOT', 'windows': "\\\\server01\\JOBS\\16002_MR_ROBOT"},
+    [newUser, newUser2],
+    schema
+)
+pprint.pprint(newProject.getExpandedSchema())
