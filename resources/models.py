3cb1c1df0b4831d2a59b788226d3b2e498acff49
robotanthony <anthony@robottalk.tv>
2016-10-19 17:32:06 -0400

    added auth_64 field to users

--- resources/models.py
+++ resources/models.py
@@ -2,8 +2,10 @@ import re
 from acl import ACL
 from pymongo.write_concern import WriteConcern
 from pymodm import MongoModel, fields
+from pymodm import errors as pymodm_errors
 import posixpath
 import pprint
+import base64
 
 def ValidateName(value):
     if ' ' in value:
@@ -24,23 +26,23 @@ def ValidateUserPermissions(value):
     diff = list(set(value) - set(permissions))
 
     if len(diff) > 0:
-        raise ValueError("Invalid user permissions specified: %s" % diff)
+        raise pymodm_errors.ValidationError("Invalid user permissions specified: %s" % diff)
 
 def ValidateProjectPaths(paths):
     try:
         paths['linux'] = posixpath.abspath(paths['linux'])
     except KeyError:
-        raise KeyError('Project paths property is missing linux propety.')
+        raise pymodm_errors.ValidationError('Project paths property is missing linux propety.')
 
     try:
         paths['darwin'] = posixpath.abspath(paths['darwin'])
     except KeyError:
-        raise KeyError('Project paths property is missing darwin property.')
+        raise pymodm_errors.ValidationError('Project paths property is missing darwin property.')
 
     try:
         paths['windows'] = posixpath.normpath(paths['windows'].replace('\\', '/'))
     except KeyError:
-        raise KeyError('Project paths property is missing windows property.')
+        raise pymodm_errors.ValidationError('Project paths property is missing windows property.')
 
 class Schema(MongoModel):
     title = fields.CharField(min_length = 3, validators=[ValidateName], required = True)
@@ -55,9 +57,15 @@ class User(MongoModel):
     username = fields.CharField(min_length = 3, validators = [ValidateName], required = True)
     permissions = fields.ListField(validators = [ValidateUserPermissions], required = True)
     password = fields.CharField(min_length = 8, required = True)
+    auth_b64 = fields.CharField(min_length = 11)
 
     class Meta:
         connection_alias = 'fsapi-app'
+
+    def clean(self):
+        auth = base64.b64encode("%s:%s" % (self.username, self.password))
+        if self.auth_b64 != auth:
+            raise pymodm_errors.ValidationError('auth_b64 must be a base64 encoded string of username:password.')
 
 class Project(MongoModel):
     title = fields.CharField(min_length=3, validators=[ValidateName], required = True)
