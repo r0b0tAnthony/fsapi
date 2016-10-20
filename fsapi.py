c3fc9f822f31a3c0d772f25c12c7f8086309f720
robotanthony <anthony@robottalk.tv>
2016-10-19 22:22:06 -0400

    Added createUser route support with user authorization, process req and response json.

--- fsapi.py
+++ fsapi.py
@@ -2,30 +2,89 @@ import falcon
 import falcon.status_codes as status
 from serve_swagger import SpecServer
 from waitress import serve
+from pymodm import connect
+from resources.models import User, Project, Schema
+import pprint
+import json
+
+def ProcessJsonResp(**request_handler_args):
+    if 'result' not in request_handler_args['req'].context:
+        return
+
+    request_handler_args['resp'].body = json.dumps(request_handler_args['req'].context['result'])
+
+def ProcessJsonReq(**request_handler_args):
+    # req.stream corresponds to the WSGI wsgi.input environ variable,
+    # and allows you to read bytes from the request body.
+    #
+    # See also: PEP 3333
+    req = request_handler_args['req']
+    if req.content_length in (None, 0):
+        # Nothing to do
+        return
+
+    body = req.stream.read()
+    if not body:
+        raise falcon.HTTPBadRequest('Empty request body',
+                                    'A valid JSON document is required.')
+
+    try:
+        req.context['doc'] = json.loads(body.decode('utf-8'))
+
+    except (ValueError, UnicodeDecodeError):
+        raise falcon.HTTPError(falcon.HTTP_753,
+                               'Malformed JSON',
+                               'Could not decode the request body. The '
+                               'JSON was incorrect or not encoded as '
+                               'UTF-8.')
+
+def RequireJson(**request_handler_args):
+    req = request_handler_args['req']
+    if not req.client_accepts_json:
+        raise falcon.HTTPNotAcceptable('This API only supports responses encoded as JSON.')
+    if req.method in ('POST', 'PUT'):
+        if 'application/json' not in req.content_type:
+            raise falcon.HTTPUnsupportedMediaType('This API only supports requests encoded as JSON.')
+
+def createUser(**request_handler_args):
+    authUser(request_handler_args['req'], request_handler_args['resp'], ['createUser'])
+    doc = request_handler_args['req'].context['doc']
+    try:
+        user = User(username = doc['username'], password = doc['password'], permissions = doc['permissions'], auth_b64 = User.GetAuthBase64(doc['username'], doc['password'])).save()
+        request_handler_args['req'].context['result'] = user.to_dict()
+    except KeyError as e:
+        raise falcon.HTTPBadRequest('Invalid User Object', "User JSON Object is invalid. %s" % e)
 
 def createFile(**request_handler_args):
         resp = request_handler_args['resp']
         resp.status = falcon.HTTP_200
         resp.body = ("%s HELP HELP" % resp.body)
-def authUser(**request_handler_args):
-    req = request_handler_args['req']
-    resp = request_handler_args['resp']
+def authUser(req, resp, permissions):
     authHeader = req.get_header('Authorization')
+    if authHeader == None:
+        raise falcon.HTTPMissingHeader('Authorization')
     resp.status = falcon.HTTP_200
-    resp.body = authHeader
+    try:
+        user = User.objects.get({"auth_b64": authHeader[6:]})
+        diff_perms = list(set(permissions) - set(User.valid_permissions))
+        if len(diff_perms) > 0:
+            raise falcon.HTTPForbidden('Forbidden', "%s does not have the required permissions: %s" % (user.username, ', '.join(diff_perms)))
+    except User.DoesNotExist:
+        raise falcon.HTTPForbidden('Forbidden','Username and password does not exist.')
+
 
 def not_found(**request_handler_args):
     raise falcon.HTTPNotFound('Not found.', 'Requested resource not found.')
 
 def forbidden(**request_handler_args):
-    raise falcon.HTTPForbidden('Forbidden.', 'You are forbidden from accessing this.')
+    raise falcon.HTTPForbidden('Forbidden', 'You are forbidden from accessing this.')
 
 def im_a_teapot(**request_handler_args):
     resp = request_handler_args['resp']
     resp.status = status.HTTP_IM_A_TEAPOT
 
 operation_handlers = {
-    'createUser':                   [not_found],
+    'createUser':                   [RequireJson, ProcessJsonReq, createUser, ProcessJsonResp],
     'updateUser':                   [not_found],
     'deleteUser':                   [not_found],
     'getUsers':                     [not_found],
@@ -34,7 +93,7 @@ operation_handlers = {
     'getProject':                   [not_found],
     'updateProject':                [not_found],
     'deleteProject':                [not_found],
-    'createFile':                   [authUser, createFile],
+    'createFile':                   [createFile],
     'getFile':                      [not_found],
     'setACL':                       [not_found],
     'getACL':                       [not_found],
@@ -47,6 +106,7 @@ operation_handlers = {
     'updateACLSchema':              [not_found],
     'deleteACLSchema':              [not_found],
 }
+connect("mongodb://localhost:27017/fsapi", alias='fsapi-app')
 
 api = application = falcon.API()
 server = SpecServer(operation_handlers=operation_handlers)
